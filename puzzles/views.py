a3b5146293e7287fc4bfd2b0edf07e0f2dd16088
Ryan Koven <celeritas@ucla.edu>
2014-01-21 00:57:06 -0500

    Modify game puzzle template and view

--- puzzles/views.py
+++ puzzles/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, render_to_response
 from puzzles.models import PuzzleCategory, Puzzle
+from random import shuffle
 
 def puzzle_board(request):
 	return render_to_response('puzzle.html', {})
 
 def puzzle(request, puzzle_id):
 	p = Puzzle.objects.get(id=puzzle_id)
-	context = {'puzzle': p}
+	answers = [ans.replace('_', ' ') for ans in p.right_answers.split() + p.wrong_answers.split()]
+	shuffle(answers)
+	context = {'puzzle': p, 'answers': answers}
 	return render_to_response('puzzle.html', context)
 
 
