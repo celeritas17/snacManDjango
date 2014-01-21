f92497f7ff131b0dbdd5e1b42e6f07c6e6e4f28b
Ryan Koven <celeritas@ucla.edu>
2014-01-21 00:14:33 -0500

    Create puzzle view, urls for games; modify css

--- puzzles/urls.py
+++ puzzles/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('puzzles.views',
 	url(r'^puzzle_board/$', 'puzzle_board'),
+	url(r'^get/(?P<puzzle_id>\d+)/$', 'puzzle')
 	)
--- puzzles/views.py
+++ puzzles/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, render_to_response
+from puzzles.models import PuzzleCategory, Puzzle
 
 def puzzle_board(request):
 	return render_to_response('puzzle.html', {})
+
+def puzzle(request, puzzle_id):
+	p = Puzzle.objects.get(id=puzzle_id)
+	context = {'puzzle': p}
+	return render_to_response('puzzle.html', context)
 
 
