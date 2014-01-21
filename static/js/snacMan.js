f28b0d5f31e5f1eb167e5e3220923c68a1aebefa
Ryan Koven <celeritas@ucla.edu>
2014-01-21 17:35:58 -0500

    Add class to muncher nav buttons, start click handlers

--- static/js/snacMan.js
+++ static/js/snacMan.js
@@ -1,4 +1,26 @@
 $(function(){
 	var cell = $('#cell_id').val();
 	$('#' + cell).toggle();
+
+	$('.nav_button').click(function(){
+		$('#' + cell).toggle();
+	});
+
+	$('#left').click(function(){
+		var new_cell;
+
+		//$('#' + new_cell).toggle();
+	});
+
+	$('#right').click(function(){
+		
+	});
+
+	$('#up').click(function(){
+		
+	});
+
+	$('#down').click(function(){
+		
+	});
 });
