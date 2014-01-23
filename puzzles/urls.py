498b1ad4a75bf5473afe8af9ffcfa271bf945cbf
Ryan Koven <celeritas@ucla.edu>
2014-01-22 19:47:18 -0500

    Styled 'scoreboard' section for base template, removed 'puzzle_board' url, view, template

--- puzzles/urls.py
+++ puzzles/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('puzzles.views',
-	url(r'^puzzle_board/$', 'puzzle_board'),
 	url(r'^get/(?P<puzzle_id>\d+)/$', 'puzzle'),
 	url(r'^all/$', 'puzzles'),
 	)
--- puzzles/views.py
+++ puzzles/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render, render_to_response
 from puzzles.models import PuzzleCategory, Puzzle
 from random import shuffle
 
-def puzzle_board(request):
-	return render_to_response('puzzle.html', {})
-
 def puzzle(request, puzzle_id):
 	p = Puzzle.objects.get(id=puzzle_id)
 	answers = [ans.replace('_', ' ') for ans in p.right_answers.split() + p.wrong_answers.split()]
